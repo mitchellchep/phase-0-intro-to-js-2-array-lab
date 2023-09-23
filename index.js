426af9193122755ac7d0b24c7caaba1603c1470c
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-23 21:59:28 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat){
+    cats.push(cat)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let newArray = [...cats];
+    newArray.push(name)
+    return newArray
+}
+
+function prependCat(name){
+    var newArray = [...cats];
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat(){
+    var newArray = [...cats];
+    newArray.pop()
+    return newArray
+}
+
+function removeFirstCat(){
+    var newArray = [...cats];
+    newArray.shift()
+    return newArray
+}
